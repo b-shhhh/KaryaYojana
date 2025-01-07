53300162dd427d11059b5aef918c4212b8e2c13c
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-01-07 08:29:06 +0545

    Authentication for navigation to the path

--- karya-yojana/src/rct/App.jsx
+++ karya-yojana/src/rct/App.jsx
@@ -1,12 +1,15 @@
-import React, { Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
 
 // Lazy-loaded pages
 const LandingPage = React.lazy(() => import('../public/LandingPage.jsx'));
 const Authentication = React.lazy(() => import('../public/Authenticaiton.jsx'));
-
+const Feature = React.lazy(() => import('../features/Feature.jsx'));
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
@@ -14,11 +17,21 @@ function App() {
           {/* Landing Page Route */}
           <Route path="/" element={<LandingPage />} />
 
-          {/* Authentication Route */}
+          {/* Jatii pani login signup wala portion xa sabb */}
           <Route path="/signup" element={<Authentication />} />
           <Route path="/login" element={<Authentication />} />
-          <Route path="/Empsignup" element={<Authentication/>}/>
-          <Route path="/EmpLogin" element={<Authentication/>}/>
+          <Route path="/Empsignup" element={<Authentication />} />
+          <Route path="/EmpLogin" element={<Authentication />} />
+
+          {/* Authenticated xain vane login*/}
+          <Route
+            path="/features"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <Feature />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Suspense>
     </Router>
