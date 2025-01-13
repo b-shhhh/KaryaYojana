7eb07464ba0703a8f92e8d88fd702057c7c07da8
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-01-13 19:21:32 +0545

    Minor changes added

--- karya-yojana/src/features/HeaderMain.jsx
+++ karya-yojana/src/features/HeaderMain.jsx
@@ -1,51 +1,56 @@
 import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/Untitled.png";
 import "../css/mainHeader.css";
-import { Link } from "react-router-dom";
+
 const HeaderMain = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const location = useLocation(); // Get the current location
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  // Helper function to check if the link is active
+  const isActive = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
+
   return (
-    <>
-      <div className="header-main">
-        <div className="main-logo">
-          <Link to="/ApplicantHome">
+    <div className="header-main">
+      <div className="main-logo">
+        <Link to="/ApplicantHome">
           <img src={logo} alt="Logo" />
-          </Link>
-        </div>
-        <div className="main-menu">
-        <ul>
-            <li>
-              <Link to="/ApplicantHome">Home</Link>
-            </li>
-            <li>
-              <Link to="/preparation">Interview Preparation</Link>
-            </li>
-            <li>
-              <Link to="/profileApplicant">Resume Builder</Link>
-            </li>
-            <li>
-              <Link to="/applications">My Applications</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="profile" onClick={toggleDropdown}>
-          <img src="../assests/person.svg"></img>
-          {isDropdownOpen && (
-            <div className="dropdown-menu">
-              <ul>
-                <li onClick={() => window.location.href = "/profileApplicant"}>Profile</li>
-                <li onClick={() => window.location.href = "/login"}>Logout</li>
-              </ul>
-            </div>
-          )}
-        </div>
+        </Link>
       </div>
-    </>
+      <div className="main-menu">
+        <ul>
+          <li className={isActive("/ApplicantHome")}>
+            <Link to="/ApplicantHome">Home</Link>
+          </li>
+          <li className={isActive("/preparation")}>
+            <Link to="/preparation">Interview Preparation</Link>
+          </li>
+          <li className={isActive("/profileApplicant")}>
+            <Link to="/profileApplicant">Resume Builder</Link>
+          </li>
+          <li className={isActive("/applications")}>
+            <Link to="/applications">My Applications</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="profile" onClick={toggleDropdown}>
+        <img src="../assests/person.svg" alt="Profile" />
+        {isDropdownOpen && (
+          <div className="dropdown-menu">
+            <ul>
+              <li onClick={() => window.location.href = "/profileApplicant"}>Profile</li>
+              <li onClick={() => window.location.href = "/login"}>Logout</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
