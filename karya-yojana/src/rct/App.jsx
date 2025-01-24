0ada3e61b6cf65f52796649aa7ac50dbc3192baf
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-01-24 11:05:20 +0545

    Added url protection

--- karya-yojana/src/rct/App.jsx
+++ karya-yojana/src/rct/App.jsx
@@ -6,78 +6,61 @@ import ProtectedRoute from './ProtectedRoute';
 const LandingPage = React.lazy(() => import('../public/LandingPage.jsx'));
 const Authentication = React.lazy(() => import('../public/Authenticaiton.jsx'));
 const Feature = React.lazy(() => import('../features/Feature.jsx'));
-
+const ProtectedRoute = ({ token, children }) => {
+  return token ? children : <Navigate to="/login" />;
+};
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("token"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+  const setTokenHandler = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setToken(newToken);
+  };
 
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           {/* Landing Page Route */}
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={token ? <Navigate to="/ApplicantHome" /> : <LandingPage />} />
 
           {/* Jatii pani login signup wala portion xa sabb */}
           <Route path="/signup" element={<Authentication />} />
-          <Route path="/login" element={<Authentication />} />
+          <Route path="/login" element={<Authentication setToken={setTokenHandler}/>} />
           <Route path="/Empsignup" element={<Authentication />} />
           <Route path="/EmpLogin" element={<Authentication />} />
-          <Route
-            path="/features"
-            element={
-              
-                <Feature />
-          
-            }
-          />
             <Route
             path="/ApplicantHome"
-            element={
-             
-                <Feature />
-          
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+          </ProtectedRoute>}/>
             <Route
             path="/profileApplicant"
-            element={
-      
-                <Feature />
-            
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+            </ProtectedRoute>}/>
             <Route
             path="/preparation"
-            element={
-        
-                <Feature />
-           
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+            </ProtectedRoute>}/>
             <Route
             path="/applications"
-            element={
-            
-                <Feature />
-           
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+            </ProtectedRoute>}/>
           <Route
             path="/appManageAcc"
-            element={
-              
-                <Feature />
-              
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+            </ProtectedRoute>}/>
             <Route
             path="/jobdesc"
-            element={
-              
-                <Feature />
-              
-            }
-          />
+            element={<ProtectedRoute token={token}><Feature />
+            </ProtectedRoute>}/>
             <Route path="/jobdesc/:jobId" element={<Feature/>} />
         </Routes>
       </Suspense>
