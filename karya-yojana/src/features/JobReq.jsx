dd234eb9407e915a497bf2c27e571e9a1b157631
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-02-05 08:51:12 +0545

    Minor change

--- karya-yojana/src/features/JobReq.jsx
+++ karya-yojana/src/features/JobReq.jsx
@@ -14,6 +14,7 @@ const JobPostingForm = () => {
     transaction: "",
   });
 
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
